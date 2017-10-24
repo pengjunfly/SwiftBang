a6b6d7e922866fcfa284f3c8dad58f72e0969040
Peng Jun <pengjun@PengdeMacBook-Pro.local>
2017-10-24 15:35:40 +0800

    Initial Commit

---
+++ SwiftBang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBang
+//
+//  Created by Peng Jun on 2017/10/24.
+//  Copyright © 2017年 Peng Jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
